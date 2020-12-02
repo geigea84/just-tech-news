a23c7059e2f49f0297bfebe0a32a635f6fa1b972
geigea84 <geigea84@gmail.com>
2020-12-01 22:04:45 -0600

    finished 13.3

--- models/index.js
+++ models/index.js
@@ -6,4 +6,17 @@ It seems unnecessary at the moment, but doing this now will set us up for future
 const User = require("./User");
 const Post = require("./Post");
 
+//3.5 create association
+User.hasMany(Post, {
+    foreignKey: "user_id"
+});
+
+//3.5 create reverse association
+Post.belongsTo(User, {
+    foreignKey: "user_id"
+});
+/* Implication that's being made with the associations:
+The User can have many Posts, but each Post can only
+have one User. */
+
 module.exports = {User, Post};
--- server.js
+++ server.js
@@ -14,6 +14,17 @@ app.use(express.urlencoded({extended: true}));
 app.use(routes);
 
 //turn on connection to db and server (connects to const defined on line 5)
+/* 3.5 In the sync method, there is a configuration parameter { force: false }. 
+If we change the value of the force property to true, then the database 
+connection must sync with the model definitions and associations. By forcing 
+the sync method to true, we will make the tables re-create if there are any 
+association changes. This definition performs similarly to DROP TABLE IF EXISTS, 
+which was used previously. This allows the table to be overwritten and re-created.
+
+Dropping all the tables every time the application restarts is no longer necessary 
+and in fact will constantly drop all the entries and seed data we enter, which can 
+get very annoying. Think of this true/false force relationship as a reset button:
+Do we need to update/sync the connections? Force true to reset, otherwise false. */
 sequelize.sync({force: false}).then(() => {
         app.listen(PORT, () => console.log("Now listening"));
 });
